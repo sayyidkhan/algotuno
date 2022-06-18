36149d3afa3944d32bb76065c3b17df7b30375f4
sayyidkhan <sayyidkhan92@hotmail.com>
2022-06-19 00:15:45 +0800

    add v5 lstm

--- app/mini_stock_app/read_from_json_file.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs-extra');
-
-const readJsonFile = async (file_directory) => {
-    const myJsonObject = await fs.readJson(file_directory);
-    console.log(myJsonObject);
-}
-
-export default readJsonFile;

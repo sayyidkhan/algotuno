e9fc635ed6c04f15ab46e343f7ebe8f4f22573ac
sayyidkhan <sayyidkhan92@hotmail.com>
2022-07-10 20:30:52 +0800

    added a delete all files api

--- app/tensorflow_v5/rnn_build_api_model_v5.js
+++ app/tensorflow_v5/rnn_build_api_model_v5.js
@@ -1,8 +1,15 @@
 import {get_price_list_for_many_stocks, get_stock_list} from "./toolsets/stock_api.js";
 import {run_build_model} from "./rnn_build_model_v5.js";
+import * as fsExtra from "fs-extra";
 
 /*** variables ***/
+const should_delete_all_existing_files = false;
 const should_rebuild_model = true;
+
+/*** delete all existing files ***/
+async function delete_all_existing_files() {
+    fsExtra.emptyDirSync('./tf_models_automation/');
+}
 
 /*** rebuild model function ***/
 async function build_api_model_v5() {
@@ -25,6 +32,12 @@ async function build_api_model_v5() {
 }
 
 
+// 1. delete all existing files
+if (should_delete_all_existing_files) {
+    delete_all_existing_files().then(res => { console.log("--- ALL FILES DELETED SUCCESSFULLY ---") })
+}
+
+// 2. rebuild model
 if (should_rebuild_model) {
     build_api_model_v5().then(res => {console.log("--- RE-BUILD MODEL COMPLETED ---")});
 }
